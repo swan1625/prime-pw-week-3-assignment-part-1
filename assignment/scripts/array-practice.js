a3e77cb1d2179a7cfb50c9848565224816f7cd7f
Andy Swanson <swan1625@icloud.com>
2021-12-17 18:32:22 -0600

    Second save week 3 part 1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -59,14 +59,17 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
-
+let removedFoods = favoriteFoods.pop();
+console.log('Removed the last food', removedFoods);
+console.log('The animals are now', favoriteFoods);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favoriteFoods.unshift('candy')
+console.log(`Added a food to beginning: ${favoriteFoods}`);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -75,3 +78,7 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
+
+removedFood = favoriteFoods.shift();
+console.log('removed the first food', removedFood);
+console.log('The Foods are now', favoriteFoods);
